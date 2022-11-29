8ac410f37087e29eb88f7c5e02b8443a62e941e7
Yuliya.Belyaeva <ybelyaeva8632@gmail.com>
2022-11-29 18:19:34 +0400

    Change Main.java

--- src/main/java/com/ybelyaeva/Main.java
+++ src/main/java/com/ybelyaeva/Main.java
@@ -2,6 +2,31 @@ package com.ybelyaeva;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        byte varByte = 125;
+        short varShort = 32000;
+        int varInt = 55000;
+        long varLong = 9100000000000000000L;
+        float varFloat = 4.5f;
+        double varDouble = 1.7d;
+        boolean varBoolean = false;
+        char varChar = 'a';
+
+        // Переполнение при вычислениях
+        System.out.println(varByte+5);
+        System.out.println(varShort+770);
+        System.out.println(varInt*1000000);
+
+        //Вычисления комбинации типов данных
+        System.out.println(varByte+varDouble);
+        System.out.println(varInt-varFloat);
+        System.out.println(varInt*varByte);
+        System.out.println(varLong/varDouble);
+
+        //Логические операторы и условная конструкция
+        if (!varBoolean && varInt > 10) {
+            System.out.println(varChar);
+        } else {
+            System.out.println("Строка не выведется в консоль, так как первое услое true");
+        }
     }
 }
